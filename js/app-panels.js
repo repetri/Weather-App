05d6931b9148791f4d327969b9ed45e57bcf39bd
ramon <ramonpetri@hotmail.com>
2018-06-07 11:35:47 +0200

    testing history graphs display  on mobile

--- js/app-panels.js
+++ js/app-panels.js
@@ -94,7 +94,8 @@ methods: {
         elements[0].scrollTo(0,0);
     },
     historyClicked: function(event){
-        App.$f7.router.navigate("/history/", options:{pushState: false});
+        App.$f7.router.clearPreviousHistory();
+        App.$f7.router.navigate("/history/", {pushState: false});
         document.dispatchEvent(new Event('newHistoryData'));
 
     },
