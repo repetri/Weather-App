bc6e3109582ccab6dcb6635fb2406f6715455dd1
ramon <ramonpetri@hotmail.com>
2018-06-07 14:46:18 +0200

    testing history graphs display  on mobile

--- js/components/history.js
+++ js/components/history.js
@@ -150,7 +150,7 @@ mounted: function(){
     }
     document.addEventListener('HistoryCall',loadHistory());
     document.addEventListener('newHistoryData', function(){
-        App.$f7.router.navigate("/history/");
+        App.$f7.router.navigate("/history/",{reloadCurrent: true});
     });
 },
 
