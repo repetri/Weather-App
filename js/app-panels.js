0e387aec257ec4d6b1cd56a37a6a6516d8e12466
ramon <ramonpetri@hotmail.com>
2018-06-13 11:42:47 +0200

    testing historygraphs on mobile

--- js/app-panels.js
+++ js/app-panels.js
@@ -22,7 +22,7 @@ var App = new Vue({
         {
             path:'/history/',
             component:'history',
-            options:'ignoreCache: true'
+            options:'data-ignore-cache="true"'
 
         },
         {
